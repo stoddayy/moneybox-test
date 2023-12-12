1249255c58601c1c61df70d345cf19336cb2588b
Michael Stoddart <m-stoddart@live.com>
2023-12-12 21:55:36 +0000

    stub interactor tests

---
+++ app/src/test/java/com/moneybox/minimb/ui/login/domain/LoginInteractorTest.kt
@@ -0,0 +1,17 @@
+package com.moneybox.minimb.ui.login.domain
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class LoginInteractorTest {
+
+    @Test
+    fun `given the request will fail, auth token repo is not updated`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `given the request will succeed, auth token repo contains the token`() {
+        TODO("Not yet implemented")
+    }
+}
