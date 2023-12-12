aca99b7dabc5b26ffe15297a12313a084cdf55a6
Michael Stoddart <m-stoddart@live.com>
2023-12-12 14:30:20 +0000

    set up activity and nav graph

--- app/src/main/java/com/moneybox/minimb/MainActivity.kt
+++ app/src/main/java/com/moneybox/minimb/MainActivity.kt
@@ -1,4 +1,13 @@
 package com.moneybox.minimb
 
-class MainActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.moneybox.minimb.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    }
 }
