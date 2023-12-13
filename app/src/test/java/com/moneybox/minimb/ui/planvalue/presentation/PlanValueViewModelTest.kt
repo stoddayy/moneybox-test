9c507d12d9a70d53ef665d1f907eb204152d02d1
Michael Stoddart <m-stoddart@live.com>
2023-12-13 12:23:33 +0000

    viewmodel tests passing

--- app/src/test/java/com/moneybox/minimb/ui/planvalue/presentation/PlanValueViewModelTest.kt
+++ app/src/test/java/com/moneybox/minimb/ui/planvalue/presentation/PlanValueViewModelTest.kt
@@ -1,6 +1,11 @@
 package com.moneybox.minimb.ui.planvalue.presentation
 
+import com.moneybox.minimb.data.networking.RequestStatus
+import com.moneybox.minimb.ui.common.RawString
+import com.moneybox.minimb.ui.planvalue.domain.UiPlanValue
 import com.moneybox.minimb.util.CoroutineTestRule
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
 
@@ -10,17 +15,47 @@ class PlanValueViewModelTest {
     val rule = CoroutineTestRule()
 
     @Test
-    fun `given the request will fail, request status is correct`() {
-        TODO("Not yet implemented")
+    fun `given the request will fail, request status is correct`() = runTest {
+        val expected = RequestStatus.FAILURE
+
+        val viewModel = createViewModel(
+            result = Result.failure(Throwable())
+        )
+
+        val actual = viewModel.uiState.value.requestStatus
+
+        assertEquals(expected, actual)
     }
 
     @Test
-    fun `given the request will succeed, request status is correct`() {
-        TODO("Not yet implemented")
+    fun `given the request will succeed, request status is correct`() = runTest {
+        val expected = RequestStatus.SUCCESS
+
+        val viewModel = createViewModel(
+            result = Result.success(UiPlanValue(planValue = RawString("£1,000.45")))
+        )
+
+        val actual = viewModel.uiState.value.requestStatus
+
+        assertEquals(expected, actual)
     }
 
     @Test
-    fun `given the request will succeed, ui state contains plan value`() {
-        TODO("Not yet implemented")
+    fun `given the request will succeed, ui state contains plan value`() = runTest {
+        val expected = RawString("£1,000.45")
+
+        val viewModel = createViewModel(
+            result = Result.success(UiPlanValue(planValue = RawString("£1,000.45")))
+        )
+
+        val actual = viewModel.uiState.value.planValue
+
+        assertEquals(expected, actual)
     }
+
+    private fun createViewModel(
+        result: Result<UiPlanValue>
+    ) = PlanValueViewModel(
+        interactor = { result }
+    )
 }
