1be8ab6ec96f76d7d77d2f67bf7e4786447afacc
Michael Stoddart <m-stoddart@live.com>
2023-12-13 12:18:37 +0000

    interactor tests passing

---
+++ app/src/test/java/com/moneybox/minimb/ui/planvalue/PlanValueFixtures.kt
@@ -0,0 +1,10 @@
+package com.moneybox.minimb.ui.planvalue
+
+import com.moneybox.minimb.data.models.products.AllProductsResponse
+
+val anAllProductsResponse = AllProductsResponse(
+    totalPlanValue = 1000.45f,
+    totalEarnings = 0f,
+    totalEarningsAsPercentage = 0f,
+    products = emptyList()
+)
--- app/src/test/java/com/moneybox/minimb/ui/planvalue/domain/PlanValueInteractorTest.kt
+++ app/src/test/java/com/moneybox/minimb/ui/planvalue/domain/PlanValueInteractorTest.kt
@@ -1,6 +1,17 @@
 package com.moneybox.minimb.ui.planvalue.domain
 
+import androidx.datastore.preferences.core.preferencesOf
+import androidx.datastore.preferences.core.stringPreferencesKey
+import com.moneybox.minimb.data.models.products.AllProductsResponse
+import com.moneybox.minimb.data.networking.AuthTokenRepository
+import com.moneybox.minimb.data.networking.LocalAuthTokenRepository
+import com.moneybox.minimb.ui.common.RawString
+import com.moneybox.minimb.ui.planvalue.anAllProductsResponse
 import com.moneybox.minimb.util.CoroutineTestRule
+import com.moneybox.minimb.util.inMemoryPrefsDataStore
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Rule
 import org.junit.Test
 
@@ -10,17 +21,52 @@ class PlanValueInteractorTest {
     val rule = CoroutineTestRule()
 
     @Test
-    fun `given the auth token repo contains no token, failure result is emitted`() {
-        TODO("Not yet implemented")
+    fun `given the auth token repo contains no token, failure result is emitted`() = runTest {
+        val interactor = createInteractor(
+            planValueResponse = Result.success(anAllProductsResponse),
+            authTokenRepository = LocalAuthTokenRepository(inMemoryPrefsDataStore())
+        )
+
+        val actual = interactor.retrieveUiPlanValue()
+
+        assertTrue(actual.isFailure)
     }
 
     @Test
-    fun `given the request will fail, failure result is emitted`() {
-        TODO("Not yet implemented")
+    fun `given the request will fail, failure result is emitted`() = runTest {
+        val interactor = createInteractor(
+            planValueResponse = Result.failure(Throwable()),
+        )
+
+        val actual = interactor.retrieveUiPlanValue()
+
+        assertTrue(actual.isFailure)
     }
 
     @Test
-    fun `given the request will succeed, a successful result is emitted with a UiPlanValue`() {
-        TODO("Not yet implemented")
+    fun `given the request will succeed, a successful result is emitted with a UiPlanValue`() = runTest {
+        val expected = Result.success(
+            UiPlanValue(planValue = RawString("£1,000.45"))
+        )
+
+        val interactor = createInteractor(
+            planValueResponse = Result.success(anAllProductsResponse),
+        )
+
+        val actual = interactor.retrieveUiPlanValue()
+
+        assertEquals(expected, actual)
     }
+
+    private fun createInteractor(
+        planValueResponse: Result<AllProductsResponse>,
+        authTokenRepository: AuthTokenRepository = LocalAuthTokenRepository(
+            inMemoryPrefsDataStore(
+                preferences = preferencesOf(stringPreferencesKey("authTokenKey") to "anAuthToken")
+            )
+        )
+    ) = RemotePlanValueInteractor(
+        planValueRepository = { planValueResponse },
+        authTokenRepository = authTokenRepository
+    )
 }
