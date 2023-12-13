197a5fd8f976e80d059fa3bb1e9f0fed9682dd39
Michael Stoddart <m-stoddart@live.com>
2023-12-13 12:08:55 +0000

    stub test cases

---
+++ app/src/test/java/com/moneybox/minimb/ui/planvalue/domain/PlanValueInteractorTest.kt
@@ -0,0 +1,26 @@
+package com.moneybox.minimb.ui.planvalue.domain
+
+import com.moneybox.minimb.util.CoroutineTestRule
+import org.junit.Rule
+import org.junit.Test
+
+class PlanValueInteractorTest {
+
+    @get:Rule
+    val rule = CoroutineTestRule()
+
+    @Test
+    fun `given the auth token repo contains no token, failure result is emitted`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `given the request will fail, failure result is emitted`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `given the request will succeed, a successful result is emitted with a UiPlanValue`() {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/test/java/com/moneybox/minimb/ui/planvalue/presentation/PlanValueViewModelTest.kt
@@ -0,0 +1,26 @@
+package com.moneybox.minimb.ui.planvalue.presentation
+
+import com.moneybox.minimb.util.CoroutineTestRule
+import org.junit.Rule
+import org.junit.Test
+
+class PlanValueViewModelTest {
+
+    @get:Rule
+    val rule = CoroutineTestRule()
+
+    @Test
+    fun `given the request will fail, request status is correct`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `given the request will succeed, request status is correct`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `given the request will succeed, ui state contains plan value`() {
+        TODO("Not yet implemented")
+    }
+}
